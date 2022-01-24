7c8f7b97ea4ee0a09f8c2e6d50c1c4b18c2a0811
Gail Mukungurutse <gail@mozzington.com>
2022-01-24 23:13:26 +0000

    practised  creating object and accessing their properties as well as creating  functions

--- session-4/index.js
+++ session-4/index.js
@@ -1,4 +1,38 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA={
+    name:'Gail', 
+    age: 45,
+    location: 'Manchester',
+    likes: 'mango',
+}
+const personB={
+    name:'Sonia', 
+    age: 25,
+    location: 'London',
+    likes: 'travelling',
+}
+const personC={
+    name:'Cii', 
+    age: 30,
+    location: 'Latvia',
+    likes: 'hunting',
+}
+const personD={
+    name:'Lavinia', 
+    age: 40,
+    location: 'Leeds',
+    likes: 'yoga',
+}
+
+function biography(person) {
+    
+    console.log('Hi! my name is ' + person.name + ' and I am ' + person.age + ' years old. I live in '+ person.location + ' and I like ' +  person.likes )
+}
+biography(personA);
+biography(personB);
+biography(personC);
+biography(personD);
+
 
 
 
@@ -19,24 +53,24 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y " + division);
 
 
 
