bbfb0a6e837eb7bc7f9cccf744dbc0349867f1be
Gail Mukungurutse <gail@mozzington.com>
2022-01-26 20:58:38 +0000

    practised with swith statement after practising with if statement

--- session-5/index.js
+++ session-5/index.js
@@ -30,3 +30,38 @@ function getGrade(total) {
     console.log('Your grade is: ' + grade);
 }
 getGrade(50);
+
+// Task2
+function getGrade(total) {
+    let grade;
+
+    switch (true) {
+        case (total > 90):
+            grade = 'A+';
+            break;
+        
+        case (total > 80):
+            grade = 'A';
+            break; 
+        case (total > 70):
+            grade = 'B';
+            break;
+        case (total > 60):
+            grade = 'C';
+            break;
+        case (total > 50):
+            grade = 'D';
+            break;
+        case(total > 40):
+            grade = 'E';
+            break
+        case (total  > 30):
+            grade = 'F';
+            break
+        default:
+            grade = 'Fail';
+    }
+
+    console.log('Your grade is: ' + grade);
+}
+getGrade(72);
