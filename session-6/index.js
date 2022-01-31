570610ed94f341889da159dfc944e5879055bfb5
Gail Mukungurutse <gail@mozzington.com>
2022-01-31 18:58:47 +0000

    worked with Loop

--- session-6/index.js
+++ session-6/index.js
@@ -3,11 +3,15 @@ const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
 
+let count= 1;
 
 
+while(count <10){
+    const randomNumber = Math.round(Math.random() * 10);
+    count = count + 1;
+    console.log(randomNumber )
 
-
-
+}
 
 
 
